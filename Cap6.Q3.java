02c4fa262b49e238744e29a62a79e8ad0dd1825f
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 12:59:17 -0300

    Update Cap6.Q3.java

--- Cap6.Q3.java
+++ Cap6.Q3.java
@@ -1,1 +1,17 @@
+package Cap6;
 
+import javax.swing.*;
+public class Questao3 {
+    public static void main(String[] args) {
+
+        double[] saldos = new double[20];
+
+        for (int i = 0; i < saldos.length; i++) {
+            saldos[i] = Double.parseDouble(JOptionPane.showInputDialog("Digite o saldo do cliente " + (i + 1)));
+        }
+
+        for (double saldo : saldos) {
+            JOptionPane.showMessageDialog(null, "Saldo: " + String.format("%.2f", saldo));
+        }
+    }
+}
