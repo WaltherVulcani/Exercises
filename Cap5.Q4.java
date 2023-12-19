33d678aca203e86fa322e8dee01087cdea6bbe16
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 12:52:33 -0300

    Update Cap5.Q4.java

--- Cap5.Q4.java
+++ Cap5.Q4.java
@@ -1,1 +1,31 @@
+package Cap5;
 
+import javax.swing.*;
+public class Questao4 {
+    public static void main(String[] args) {
+        
+            String input = JOptionPane.showInputDialog("Digite o número de iterações para calcular Pi:");
+            int iteracoes = Integer.parseInt(input);
+
+       
+            double piAproximado = calculaPi(iteracoes);
+
+            JOptionPane.showMessageDialog(null, "Valor aproximado de Pi: " + piAproximado);
+        }
+
+        private static double calculaPi(int iteracoes) {
+            double pi = 0.0;
+
+            for (int i = 0; i < iteracoes; i++) {
+                double termo = 4.0 / (2 * i + 1);
+
+                if (i % 2 == 0) {
+                    pi += termo;
+                } else {
+                    pi -= termo;
+                }
+            }
+
+            return pi;
+    }
+}
