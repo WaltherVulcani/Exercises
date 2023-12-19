a32e32136acf9beeec7f2c441e99a2d619c57692
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 12:53:38 -0300

    Update Cap5.Q6.java

--- Cap5.Q6.java
+++ Cap5.Q6.java
@@ -1,1 +1,19 @@
+package Cap5;
 
+import javax.swing.*;
+public class Questao6 {
+    public static void main(String[] args) {
+        
+            String inputBase = JOptionPane.showInputDialog("Digite a base (X):");
+            double base = Double.parseDouble(inputBase);
+
+            String inputExpoente = JOptionPane.showInputDialog("Digite o expoente (Y):");
+            int expoente = Integer.parseInt(inputExpoente);
+
+            double resultado = Math.pow(base, expoente);
+
+            JOptionPane.showMessageDialog(null, base + " elevado a " + expoente + " é: " + resultado);
+        }
+    }
+
+}
