aeb90d2cd7110dd29038cce90afbe10f0038c73c
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 12:55:40 -0300

    Update Cap5.Q9.java

--- Cap5.Q9.java
+++ Cap5.Q9.java
@@ -1,1 +1,18 @@
+package Cap5;
 
+import javax.swing.JOptionPane;
+public class Questao9 {
+    public static void main(String[] args) {
+
+=        int n = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de termos da série:"));
+        double soma = 0;
+
+        for (int i = 1; i <= n; i++) {
+            double termo = (i * i) / (i + 1);
+
+            soma += termo;
+        }
+
+        JOptionPane.showMessageDialog(null, "A soma da série é: " + soma);
+    }
+}
