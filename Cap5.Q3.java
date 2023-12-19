1df71a6f851cbeabe6f51dc076d61dd46eb74e8d
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 12:51:43 -0300

    Update Cap5.Q3.java

--- Cap5.Q3.java
+++ Cap5.Q3.java
@@ -1,1 +1,18 @@
+package Cap5;
 
+import javax.swing.JOptionPane;
+public class Questao3{
+    public static void main(String[] args) {
+       
+            String input = JOptionPane.showInputDialog("Digite o valor de N:");
+            int n = Integer.parseInt(input);
+
+            double soma = 0;
+
+            for (int i = 1; i <= n; i++) {
+                soma += 1.0 / (2 * i);
+            }
+
+            JOptionPane.showMessageDialog(null, "A soma da série para N = " + n + " é: " + soma);
+    }
+}
