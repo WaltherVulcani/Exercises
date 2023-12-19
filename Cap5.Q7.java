7fbec522a6b1e702891e7c252386d72256ff38e9
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 12:54:27 -0300

    Update Cap5.Q7.java

--- Cap5.Q7.java
+++ Cap5.Q7.java
@@ -1,1 +1,27 @@
+package Cap5;
 
+import javax.swing.JOptionPane;
+
+public class Questao7 {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i < 20) {
+            i += 2;
+
+            JOptionPane.showMessageDialog(null, "Passei por aqui!");
+
+            quad(1);
+
+            JOptionPane.showMessageDialog(null, 13);
+
+            JOptionPane.showMessageDialog(null, "Ufa, cheguei aqui!");
+        }
+    }
+
+    public static void quad(int x) {
+        JOptionPane.showMessageDialog(null, x * x);
+    }
+}
