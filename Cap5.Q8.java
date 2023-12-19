59551be9e6b23df3cc2d4229ab32e8a615220604
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 12:55:07 -0300

    Update Cap5.Q8.java

--- Cap5.Q8.java
+++ Cap5.Q8.java
@@ -1,1 +1,17 @@
+package Cap5;
 
+import javax.swing.JOptionPane;
+public class Questao8 {
+    public static void main(String[] args) {
+
+        int x = 30;
+        int y = 0;
+
+        while (x < 10) {
+            x++;
+            y--;
+
+            JOptionPane.showMessageDialog(null, x + " " + y);
+        }
+    }
+}
