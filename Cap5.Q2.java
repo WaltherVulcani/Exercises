8c15eb7dc99e8d735a830c952d63185b8d7a2cee
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 12:51:14 -0300

    Update Cap5.Q2.java

--- Cap5.Q2.java
+++ Cap5.Q2.java
@@ -1,1 +1,22 @@
+package Cap5;
 
+import javax.swing.JOptionPane;
+public class Questao2 {
+
+    public static void main(String[] args) {
+
+        int limite = 100;
+        StringBuilder resultado = new StringBuilder();
+
+        for (int i = 1; i * i <= limite; i++) {
+            int quadrado = i * i;
+            resultado.append(quadrado);
+
+            if (i * i < limite) {
+                resultado.append(", ");
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, resultado.toString());
+    }
+}
