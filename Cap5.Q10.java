b3cb8f8114d4592aab8281b29f6c4c84d7514148
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 12:50:53 -0300

    Update Cap5.Q10.java

--- Cap5.Q10.java
+++ Cap5.Q10.java
@@ -1,1 +1,23 @@
+package Cap5;
 
+import javax.swing.JOptionPane;
+public class Questao10 {
+public static void main(String[] args) {
+   
+
+            int quantidadeNegativos = 0;
+
+            for (int i = 0; i < 5; i++) {
+               
+                String input = JOptionPane.showInputDialog("Digite o valor " + (i + 1) + ":");
+                double valor = Double.parseDouble(input);
+
+                if (valor < 0) {
+                    quantidadeNegativos++;
+                }
+            }
+
+            JOptionPane.showMessageDialog(null, "Quantidade de valores negativos: " + quantidadeNegativos);
+        }
+    }
+}
