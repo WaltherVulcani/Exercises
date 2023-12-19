950791d50bb359dded771535fff9199759364540
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 12:53:09 -0300

    Update Cap5.Q5.java

--- Cap5.Q5.java
+++ Cap5.Q5.java
@@ -1,1 +1,14 @@
+package Cap5;
 
+import javax.swing.*;
+public class Questao5 {
+    public static void main(String[] args) {
+           
+            String input = JOptionPane.showInputDialog("Digite um número:");
+            double numero = Double.parseDouble(input);
+
+            double resultado = Math.pow(numero, 5);
+
+            JOptionPane.showMessageDialog(null, "O número elevado à quinta potência é: " + resultado);
+    }
+}
