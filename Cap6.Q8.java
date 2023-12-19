2e93c312e6054e2170b64ef751aa27d488bd4d72
Walther Diego <WaltherVulcani@gmail.com>
2023-12-19 13:02:29 -0300

    Update Cap6.Q8.java

--- Cap6.Q8.java
+++ Cap6.Q8.java
@@ -1,1 +1,26 @@
+package Cap6;
 
+import javax.swing.JOptionPane;
+
+public class Questao8 {
+    public static void main(String[] args) {
+      
+        int numDisciplinas = 5;
+        int numNotasPorDisciplina = 4;
+
+        double notaMaisAlta = Double.MIN_VALUE;
+
+        for (int i = 1; i <= numDisciplinas; i++) {
+            for (int j = 1; j <= numNotasPorDisciplina; j++) {
+                String input = JOptionPane.showInputDialog("Digite a nota " + j + " da disciplina " + i + ":");
+
+                double nota = Double.parseDouble(input);
+
+                if (nota > notaMaisAlta) {
+                    notaMaisAlta = nota;
+                }
+            }
+        }
+        JOptionPane.showMessageDialog(null, "A nota mais alta é: " + notaMaisAlta);
+    }
+}
